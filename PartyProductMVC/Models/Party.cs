107cea088b4a4c09ed688a7ae7ab76bd57d0d22b
Yash4Parmar <yash4parmar12@gmail.com>
2023-11-05 20:31:44 +0530

    Add project files.

---
+++ PartyProductMVC/Models/Party.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyProductMVC.Models
+{
+    public class Party
+    {
+        public int Id { get; set; }
+        public string PartyName { get; set; }
+    }
+}
---
+++ PartyProductMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartyProductMVC.Startup))]
+namespace PartyProductMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
